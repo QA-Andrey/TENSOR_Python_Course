0d4d23012283dffd6701d5bb765f3def37193a1e
QA-Andrey <andrey_popov@hotmail.com>
2023-05-19 00:16:03 +0300

    ДЗ 5

---
+++ HW_5_with_star.py
@@ -0,0 +1,49 @@
+# Напишите функцию to_roman, которая преобразуют арабское число (val) в римское (roman_str).
+#
+# Современные римские цифры записываются, выражая каждую цифру отдельно,
+# начиная с самой левой цифры и пропуская цифру со значением нуля.
+# Римскими цифрами 1990 отображается: 1000=М, 900=СМ, 90=ХС; в результате MCMXC.
+# 2023 записывается как 2000=MM, 20=XX, 3=III; или MMXXIII.
+# В 1666 используется каждый римский символ в порядке убывания: MDCLXVI.
+#
+# Например (Ввод --> Вывод) :
+# 2008 --> MMVIII
+
+
+def to_roman(val):
+    rev_val = (str(val))[::-1]
+    dict_0 = {1: 'I', 2: 'II', 3: 'III', 4: 'IV', 5: 'V', 6: 'VI', 7: 'VII', 8: 'VIII', 9: 'IX'}
+    dict_1 = {1: 'X', 2: 'XX', 3: 'XXX', 4: 'XL', 5: 'L', 6: 'LX', 7: 'LXX', 8: 'LXXX', 9: 'XC'}
+    dict_2 = {1: 'C', 2: 'CC', 3: 'CCC', 4: 'CD', 5: 'D', 6: 'DC', 7: 'DCC', 8: 'DCCC', 9: 'CM'}
+    dict_3 = {1: 'M', 2: 'MM', 3: 'MMM', 4: 'IV'}
+    roman_str = ''
+
+    for index, value in enumerate(str(rev_val)):
+        if int(value) > 0:
+            if index == 0:
+                roman_str = dict_0[int(value)] + roman_str
+            elif index == 1:
+                roman_str = dict_1[int(value)] + roman_str
+            elif index == 2:
+                roman_str = dict_2[int(value)] + roman_str
+            elif index == 3:
+                roman_str = dict_3[int(value)] + roman_str
+        elif int(value) == 0:
+            continue
+
+    return roman_str
+
+# Ниже НИЧЕГО НЕ НАДО ИЗМЕНЯТЬ
+
+
+data = [1133, 2224, 1938, 1817, 2505, 391, 3743, 1634, 699, 1666, 1494, 1444]
+
+test_data = [
+    "MCXXXIII", "MMCCXXIV", "MCMXXXVIII", "MDCCCXVII", "MMDV", "CCCXCI", 'MMMDCCXLIII', 'MDCXXXIV', 'DCXCIX', 'MDCLXVI',
+    'MCDXCIV', 'MCDXLIV']
+
+
+for i, d in enumerate(data):
+    assert to_roman(d) == test_data[i], f'С набором {d} есть ошибка, не проходит проверку'
+    print(f'Тестовый набор {d} прошёл проверку')
+print('Всё ок')
