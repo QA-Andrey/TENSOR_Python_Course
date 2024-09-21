e265f34d3b41f74a808271759b4a26185f5c9616
QA-Andrey <andrey_popov@hotmail.com>
2024-09-21 14:54:56 +0700

    Homework WB10

---
+++ test_W10_task2.py
@@ -0,0 +1,36 @@
+import pytest
+
+
+def all_division(*arg1):
+
+    division = arg1[0]
+    for i in arg1[1:]:
+        division /= i
+    return division
+
+
+@pytest.mark.smoke
+def test_smoke():
+    assert all_division(2, -3, 4.15) == -0.16064257028112447
+
+
+@pytest.mark.smoke
+def test_division_by_zero():
+    with pytest.raises(ZeroDivisionError):
+        all_division(2, 0, 4)
+
+
+def test_str():
+    with pytest.raises(TypeError):
+        all_division('4', 8, 20)
+
+
+
+def test_long_num():
+    assert all_division(123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890) == 123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890
+
+
+def test_many_numbers():
+    assert all_division(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27) == 9.183689863795547e-29
+
+
